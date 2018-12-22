4e5b4266d44e0fcad3fb81349663daaa6faa9544
shanshuirenjia <978987373@qq.com>
2018-12-22 10:08:59 +0800

    update tree structure

---
+++ frontend/src/data-structure/tree-node.js
@@ -0,0 +1,90 @@
+class TreeNode {
+  constructor() {
+    this.id = '';
+    this.parentId = '';
+    this.object = null;
+    this.parentNode = null;
+    this.childrenNode = [];
+    this.loadPath = '';
+    this.isLeaf = false;
+    this.isLoaded = false;
+    this.isExpanded = false;
+  }
+
+  getNodeById(id) {
+
+  }
+
+  getParentNode() {
+
+  }
+
+  getChildrenNodeList() {
+
+  }
+
+  getChildrenObjectList() {
+
+  }
+
+  addNode(node) {
+
+  }
+
+  addNodeList(nodeList) {
+
+  }
+
+  addNodeToParent(node, parentNode) {
+
+  }
+
+  addNodeListToParent(nodeList, parentNode) {
+
+  }
+
+  deleteNode(node) {
+
+  }
+
+  deleteNodeList(nodeList) {
+
+  }
+
+  removeNodeFromParent(node, parentNode) {
+
+  }
+
+  removeNodeListFromParent(nodeList, parentList) {
+
+  }
+
+  moveNodeToAnotherNode(node, destNode) {
+
+  }
+
+  copyNodeToAnotherNode(node, destNode) {
+
+  }
+
+  loadedNode() {
+
+  }
+
+  expandNode() {
+    
+  }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+}
